74ef5bb0bf5986c1fe27f1548b69f2d57ac1f57f
Muddhulur-Swathi <swathiraju27@gmail.com>
2020-11-05 21:46:31 +0530

    Final app

---
+++ build/precache-manifest.43674e864203e11bb0a1c8f2df51fac5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e270b77314baf51116ec7e418ce8aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d4f189b09b02a4d5e0b",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "4b4adc7cc2be01b31b7d",
+    "url": "/static/css/main.e12d5b47.chunk.css"
+  },
+  {
+    "revision": "8d4f189b09b02a4d5e0b",
+    "url": "/static/js/2.22672e0e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.22672e0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4adc7cc2be01b31b7d",
+    "url": "/static/js/main.86c01573.chunk.js"
+  },
+  {
+    "revision": "3c7beecf55bdb387b928",
+    "url": "/static/js/runtime-main.d3f8a8db.js"
+  }
+]);
